353ac0293c6220a7d19be36eb6c41a01a48ff374
Kenzy B <kenzy.bouchema@gmail.com>
2020-10-16 21:45:53 +0200

    MerngStack : Ajout du chiffrement du mot de passe lors de la création de l'utilisateur

--- graphQL/resolvers/users.js
+++ graphQL/resolvers/users.js
@@ -1,14 +1,19 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const User = require('../../models/User')
+
 // Ici on impléemente ce qu'on a défini dans le fichier typeDefs.js
 module.exports = {
     Mutation:{
-        register(parent, 
-                {username, password, confirmPassword, email}, 
-                context, 
-                info){
+        async register( // async car elle retourne une promesse
+            parent, 
+            {username, password, confirmPassword, email}, 
+            context, 
+            info){
             // TODO : Valider les données de l'user
             // TODO : S'assurer que l'utilisateur n'existe pas
             // TODO : Crypter le mot de passe
+            password = await bcrypt.hash(password,12); // bcryptJS est une promesse
         }
     }
 } 
