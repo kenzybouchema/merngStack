86d44575c4e3a974e5a27308e484d8608ae2f234
Kenzy B <kenzy.bouchema@gmail.com>
2020-10-16 21:18:05 +0200

    MerngStack : On destructure les arguments de la Mutation, registrer

--- graphQL/resolvers/users.js
+++ graphQL/resolvers/users.js
@@ -2,10 +2,13 @@ const User = require('../../models/User')
 // Ici on impléemente ce qu'on a défini dans le fichier typeDefs.js
 module.exports = {
     Mutation:{
-        register(parent, args, context, info){
+        register(parent, 
+                {username, password, confirmPassword, email}, 
+                context, 
+                info){
             // TODO : Valider les données de l'user
             // TODO : S'assurer que l'utilisateur n'existe pas
             // TODO : Crypter le mot de passe
         }
     }
-}
+} 
